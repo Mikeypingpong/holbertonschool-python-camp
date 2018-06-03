83e5015c74aee69e8f592ca5886775bcc96c31ca
Sid Grover <sidgrover@live.com>
2018-06-03 22:14:54 +0000

    initial commit

--- 0x01-python_hello_world/5-print_first_letter.py
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/python3
-my_word = "Holberton"
-print("{0} is the first letter of the word {1}".format(my_word[0], my_word))
---
+++ 0x01-python_hello_world/6-print_last_letter.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+my_word = "Holberton"
+print("{0} is the last letter of the word {1}".format(my_word[-1], my_word))
