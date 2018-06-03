da486cc9ac3593f127c865619a9abbf452c4dd05
Sid Grover <sidgrover@live.com>
2018-06-03 21:35:03 +0000

    Change print issue

--- 0x01-python_hello_world/5-print_first_letter.py
+++ 0x01-python_hello_world/5-print_first_letter.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 my_word = "Holberton"
-print(my_word[0], "is the first letter of the word {}".format(my_word))
+print( "{0} is the first letter of the word {1}".format(my_word[0], my_word))
 
